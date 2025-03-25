b4fa2521d8b64bce1cf26628ae2183d663ad2489
Ridvan <renis@arcpaygate.com>
2025-03-25 14:35:14 +0300

    lazy plugin import order changed

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,15 +17,15 @@ require("lazy").setup({
     -- import LazyVim plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     
-    -- import/override with your plugins
-    { import = "plugins" },
-    
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.go" },
+    
+    -- import/override with your plugins
+    { import = "plugins" },
     
     -- Replace the copilot import with direct plugin specification
     {
